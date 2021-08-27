108af73aa9a8aefb785f1d1f64d23912973a7523
josfletcher <89667075+josfletcher@users.noreply.github.com>
2021-08-27 18:15:07 -0400

    First Upload of Lab Three Files

---
+++ ver1/Account.java
@@ -0,0 +1,35 @@
+package ver1;
+
+public class Account {
+	private double balance;
+	public double getBalance() {
+		return balance;
+	}
+	public Account(double initBalance) {
+		balance = initBalance;
+	}
+	public void deposit(double amount) {
+		if(amount > 0) {
+			balance += amount;
+		}
+	}
+	public void withdraw(double amount) {
+		if(amount > 0) {
+			balance -= amount;
+		}
+	}
+	public String toString() {
+		String msg = "balance=$" + balance;
+		return msg;
+	}
+	public static void main(String[] args) {
+		Account a1 = new Account(1000.00);
+		System.out.println("Balance=$" + a1.getBalance());
+		a1.deposit(500.00);
+		System.out.println("Balance=$" + a1.getBalance());
+		a1.withdraw(200.00);
+		System.out.println("Balance=$" + a1.getBalance());
+		System.out.println(a1.toString());
+		System.out.println(a1);
+	}
+}
